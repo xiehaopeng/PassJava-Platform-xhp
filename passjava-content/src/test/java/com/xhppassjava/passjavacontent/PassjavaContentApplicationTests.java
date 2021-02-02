56a84058d561dde04dca863be32a326f102740cd
xiehaopeng <xhpzww0822@163.com>
2021-02-02 15:04:50 +0800

    创建项目和添加模块

---
+++ passjava-content/src/test/java/com/xhppassjava/passjavacontent/PassjavaContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhppassjava.passjavacontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
